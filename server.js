0fa48c48ab58eaaadecbdafb16781cee662854df
CieBie01 <brandontanyanyiwa5@gmail.com>
2025-08-08 00:20:13 +0200

    server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ try{
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("Connected to MongoDB");  
+  app.get('/', (req, res) => {
   res.send({ status: "Connected to MongoDB" });
+});
 })
 }catch (error) {
   console.error("Database connection error:", error);
