cf484bf9c7d89de8847c65ad32d7e8263a4b7eb0
CieBie01 <brandontanyanyiwa5@gmail.com>
2025-08-08 09:47:08 +0200

    Fix Contact api

--- Router/Router.js
+++ Router/Router.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
 import RateController from "../Controller/RateController.js";
-
+import ContactController from "../Controller/ContactController.js";
 
 
 
 router.post("/rate", RateController);
-
+router.post("/contact", ContactController);
 export default router;
