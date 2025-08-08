704aebd45c3d9d92ca59e582767bec74bd0c12f3
CieBie01 <brandontanyanyiwa5@gmail.com>
2025-08-08 02:04:09 +0200

    Get error

--- Router/Router.js
+++ Router/Router.js
@@ -1,11 +1,10 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import RateController from '../Controller/RateController.js';
+import RateController from "../Controller/RateController.js";
 
+router.get("/", (req, res) => {
+  res.send({ status: "API is working" });
+});
+router.post("/rate", RateController);
 
-router.post('/rate', RateController);
-router.get('/', (req, res) => {
-    res.send({ status: "API is working" });
-}
-);
 export default router;
