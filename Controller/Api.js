02bbeeccd3bcc5da058c728bb03653b615ccb7ca
CieBie01 <brandontanyanyiwa5@gmail.com>
2025-08-08 02:08:48 +0200

    Fixing error

---
+++ Controller/Api.js
@@ -0,0 +1,6 @@
+
+const Api =(req, res) => {
+  res.send({ status: "API is working" });
+}
+
+export default Api;
--- Router/Router.js
+++ Router/Router.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
 import RateController from "../Controller/RateController.js";
+import Api from "../Controller/Api.js";
 
-router.get("/", (req, res) => {
-  res.send({ status: "API is working" });
-});
+
+router.get("/", Api);
 router.post("/rate", RateController);
 
 export default router;
